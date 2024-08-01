c7ad75231615a5fc83eef3639644e2acc08553cb
Pedro Giacomin <plsg2020.ufmg@gmail.com>
2024-08-01 08:40:16 -0300

    Último commit antes do merge

--- scada.py
+++ scada.py
@@ -1,6 +1,11 @@
 import socket
 import threading as th
 import time
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 PORT = 1919
 HOST = 'localhost'
@@ -10,6 +15,41 @@ TAXA_AQ = 2 # em segundos
 T_SP = 10.0
 T = 0.0
 Q = 0.0
+
+class RealTimeGraphApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Gráfico de Tendência em Tempo Real")
+        
+        self.frame = ttk.Frame(root)
+        self.frame.pack(fill=tk.BOTH, expand=1)
+        
+        self.fig, self.ax = plt.subplots()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)
+        self.canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1)
+
+        self.x_data = []
+        self.y_data = []
+        
+        self.ani = FuncAnimation(self.fig, self.update_plot, interval=1000)
+        
+    def update_plot(self, frame):
+        global T
+        self.x_data.append(time.time())
+        self.y_data.append(T)
+        
+        # Limita o gráfico aos últimos 20 pontos
+        self.x_data = self.x_data[-20:]
+        self.y_data = self.y_data[-20:]
+        
+        self.ax.clear()
+        self.ax.plot(self.x_data, self.y_data, marker='o', color='b')
+        
+        # Formata o eixo x para mostrar o tempo de forma legível
+        self.ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: time.strftime('%H:%M:%S', time.localtime(x))))
+        
+        self.fig.autofmt_xdate()
+
 
 # O clienteTCP envia apenas um set_point float
 def start_client_tcp():
@@ -64,6 +104,16 @@ if __name__ == "__main__":
 
     tcp_thread = th.Thread(target=start_client_tcp)
     tcp_thread.start()
+
+    # # Plota interface
+    # tq_mutex.acquire()
+    # T_aux = T
+    # Q_aux = Q
+    # tq_mutex.release()
+    
+    # root = tk.Tk()
+    # app = RealTimeGraphApp(root)
+    # root.mainloop()
 
     tcp_thread.join()
 
--- teste.py
+++ teste.py
@@ -6,75 +6,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import random
 import time
 
-class RealTimeGraphApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Gráficos de Tendência em Tempo Real")
 
-        # Configuração do layout em grade
-        self.frame = ttk.Frame(root)
-        self.frame.grid(row=0, column=0, sticky="nsew")
-
-        # Criação das figuras e subplots
-        self.fig, (self.ax1, self.ax2) = plt.subplots(2, 1)
-        self.fig.tight_layout(pad=3.0)
-
-        # Canvas para o primeiro gráfico
-        self.canvas1 = FigureCanvasTkAgg(self.fig, master=self.frame)
-        self.canvas1.get_tk_widget().grid(row=0, column=0, rowspan=2, columnspan=2, sticky="nsew")
-
-        # Canvas para o segundo gráfico (usando a mesma figura)
-        self.canvas2 = FigureCanvasTkAgg(self.fig, master=self.frame)
-        self.canvas2.get_tk_widget().grid(row=2, column=0, rowspan=1, columnspan=2, sticky="nsew")
-
-        # Dados dos gráficos
-        self.x_data = []
-        self.y1_data = []
-        self.y2_data = []
-
-        # Animações para os gráficos
-        self.ani1 = FuncAnimation(self.fig, self.update_plot1, interval=1000)
-        self.ani2 = FuncAnimation(self.fig, self.update_plot2, interval=1000)
-
-        # Configuração das linhas e colunas do grid
-        for i in range(3):
-            self.frame.rowconfigure(i, weight=1)
-        for i in range(2):
-            self.frame.columnconfigure(i, weight=1)
-
-    def update_plot1(self, frame):
-        self.x_data.append(time.time())
-        self.y1_data.append(random.randint(0, 100))
-
-        # Limita os dados aos últimos 20 pontos
-        self.x_data = self.x_data[-20:]
-        self.y1_data = self.y1_data[-20:]
-
-        self.ax1.clear()
-        self.ax1.plot(self.x_data, self.y1_data, marker='o', color='b')
-        self.ax1.set_title('Gráfico 1')
-
-        # Formata o eixo x para mostrar o tempo de forma legível
-        self.ax1.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: time.strftime('%H:%M:%S', time.localtime(x))))
-        self.fig.autofmt_xdate()
-
-    def update_plot2(self, frame):
-        self.x_data.append(time.time())
-        self.y2_data.append(random.randint(0, 100))
-
-        # Limita os dados aos últimos 20 pontos
-        self.x_data = self.x_data[-20:]
-        self.y2_data = self.y2_data[-20:]
-
-        self.ax2.clear()
-        self.ax2.plot(self.x_data, self.y2_data, marker='o', color='r')
-        self.ax2.set_title('Gráfico 2')
-
-        # Formata o eixo x para mostrar o tempo de forma legível
-        self.ax2.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: time.strftime('%H:%M:%S', time.localtime(x))))
-        self.fig.autofmt_xdate()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = RealTimeGraphApp(root)
-    root.mainloop()
+    
